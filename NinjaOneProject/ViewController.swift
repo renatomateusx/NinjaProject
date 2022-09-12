d8efb7a1dc00c4fc2822d76b2b40c928c4bc70a9
renatomateusx <renatomateusx@gmail.com>
2022-09-12 18:17:33 -0300

    Initial Commit

---
+++ NinjaOneProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NinjaOneProject
+//
+//  Created by Renato Mateus on 12/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
